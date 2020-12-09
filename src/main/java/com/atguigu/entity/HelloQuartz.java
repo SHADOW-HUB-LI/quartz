a9b7b012ad28e30c70bfedd1a0486a398c2f5dfd
shadow0821 <shaow@163.com>
2020-12-09 12:38:01 +0800

    test02

---
+++ src/main/java/com/atguigu/entity/HelloQuartz.java
@@ -0,0 +1,16 @@
+package com.atguigu.entity;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HelloQuartz implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(new Date()));
+    }
+}
